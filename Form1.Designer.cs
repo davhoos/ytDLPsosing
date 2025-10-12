c997ea8aed7e8d823fcdfc4d67ca73cbc818723a
davhoos <hor.dav@seznam.cz>
2025-10-12 11:46:02 +0200

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace ytDLPsosing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rtbOUTPUT = new System.Windows.Forms.RichTextBox();
+            this.txbURL = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnKillProcess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.btnStartDownload = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.checkMusicOrVideo = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // rtbOUTPUT
+            // 
+            this.rtbOUTPUT.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.rtbOUTPUT.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rtbOUTPUT.Location = new System.Drawing.Point(4, 191);
+            this.rtbOUTPUT.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rtbOUTPUT.Name = "rtbOUTPUT";
+            this.rtbOUTPUT.ReadOnly = true;
+            this.rtbOUTPUT.Size = new System.Drawing.Size(882, 436);
+            this.rtbOUTPUT.TabIndex = 0;
+            this.rtbOUTPUT.Text = "";
+            // 
+            // txbURL
+            // 
+            this.txbURL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txbURL.Location = new System.Drawing.Point(18, 23);
+            this.txbURL.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbURL.Name = "txbURL";
+            this.txbURL.Size = new System.Drawing.Size(686, 33);
+            this.txbURL.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(481, 86);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(222, 38);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "clear outputs";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(478, 57);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(205, 29);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "download Playlist";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            // 
+            // btnKillProcess
+            // 
+            this.btnKillProcess.Location = new System.Drawing.Point(18, 134);
+            this.btnKillProcess.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKillProcess.Name = "btnKillProcess";
+            this.btnKillProcess.Size = new System.Drawing.Size(129, 35);
+            this.btnKillProcess.TabIndex = 5;
+            this.btnKillProcess.Text = "Kill download !";
+            this.btnKillProcess.UseVisualStyleBackColor = true;
+            this.btnKillProcess.Visible = false;
+            this.btnKillProcess.Click += new System.EventHandler(this.btnKillProcess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(362, 634);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "D. Horsky - 2025 v0.6";
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(214, 85);
+            this.btnOpenFolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(256, 85);
+            this.btnOpenFolder.TabIndex = 8;
+            this.btnOpenFolder.Text = "Open download folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // btnStartDownload
+            // 
+            this.btnStartDownload.BackgroundImage = global::ytDLPsosing.Properties.Resources.youtube_256x256;
+            this.btnStartDownload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnStartDownload.Location = new System.Drawing.Point(714, 18);
+            this.btnStartDownload.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStartDownload.Name = "btnStartDownload";
+            this.btnStartDownload.Size = new System.Drawing.Size(164, 151);
+            this.btnStartDownload.TabIndex = 1;
+            this.btnStartDownload.Text = "Download";
+            this.btnStartDownload.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnStartDownload.UseVisualStyleBackColor = true;
+            this.btnStartDownload.Click += new System.EventHandler(this.btnStartDownload_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(481, 134);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(222, 35);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "update kernel";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // checkMusicOrVideo
+            // 
+            this.checkMusicOrVideo.AutoSize = true;
+            this.checkMusicOrVideo.Checked = true;
+            this.checkMusicOrVideo.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkMusicOrVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkMusicOrVideo.Location = new System.Drawing.Point(18, 105);
+            this.checkMusicOrVideo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkMusicOrVideo.Name = "checkMusicOrVideo";
+            this.checkMusicOrVideo.Size = new System.Drawing.Size(170, 29);
+            this.checkMusicOrVideo.TabIndex = 10;
+            this.checkMusicOrVideo.Text = "Video / Music";
+            this.checkMusicOrVideo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 660);
+            this.Controls.Add(this.checkMusicOrVideo);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKillProcess);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txbURL);
+            this.Controls.Add(this.btnStartDownload);
+            this.Controls.Add(this.rtbOUTPUT);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ytDLP sosing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbOUTPUT;
+        private System.Windows.Forms.Button btnStartDownload;
+        private System.Windows.Forms.TextBox txbURL;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnKillProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.CheckBox checkMusicOrVideo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq.Expressions;
+using System.Windows.Forms;
+
+namespace ytDLPsosing
+{
+    public partial class Form1 : Form
+    {
+        private Process cmd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region FORM LOAD
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("yt-dlp.exe"))
+            {
+                MessageBox.Show("Integrity check failed - missing yt-dlp.exe!");
+            }
+
+
+            // Optional: Set label if not already set in the designer
+            checkMusicOrVideo.Text = "Video or Music";
+
+        }
+        #endregion
+
+
+        // Create a new folder for downloads
+        string myVideosPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "ytdlp_videos") + Path.DirectorySeparatorChar;
+
+
+
+        // Check if Music or Video should be downloaded
+        string formatOption;
+
+
+
+
+        #region StartDownloadVideo
+        private void StartDownloadVideo()
+        {
+            try
+            {
+                if (!Directory.Exists(myVideosPath))
+                {
+                    Directory.CreateDirectory(myVideosPath);
+                }
+
+                // Build correct format options
+                formatOption = "-f \"bestvideo[ext=mp4][height<=1440]+bestaudio[ext=m4a]/best[ext=mp4]\"";
+                string outputTemplate = Path.Combine(myVideosPath, "%(title)s.%(ext)s");
+
+                // Full yt-dlp arguments
+                string arguments = $"{formatOption} --merge-output-format mp4 --remux-video mp4 --no-keep-video -o \"{outputTemplate}\" \"{txbURL.Text}\"";
+
+                cmd = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "yt-dlp.exe",
+                        Arguments = arguments,
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    },
+                    EnableRaisingEvents = true
+                };
+
+                cmd.OutputDataReceived += Cmd_OutputDataReceiver;
+                cmd.ErrorDataReceived += Cmd_OutputDataReceiver;
+                cmd.Exited += Cmd_ProcessExited;
+
+                cmd.Start();
+                cmd.BeginOutputReadLine();
+                cmd.BeginErrorReadLine();
+
+                AppendOutput($" Video Download started... Files saved in folder: {myVideosPath}\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+        //
+
+        private void StartDownloadMusic()
+        {
+
+            try
+            {
+
+                formatOption = "-f bestaudio --extract-audio --audio-format mp3";
+
+                string outputTemplate = Path.Combine(myVideosPath, "%(title)s.%(ext)s");
+
+                cmd = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "yt-dlp.exe",
+                        Arguments = $"{txbURL.Text} {formatOption} -o \"{outputTemplate}\"",                                                                                                                       //Arguments = $"{txbURL.Text} {playlistOption} {formatOption} -o \"{outputPath}\"", //for mp3 
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    }
+                };
+
+
+                cmd.OutputDataReceived += Cmd_OutputDataReceiver;
+                cmd.ErrorDataReceived += Cmd_OutputDataReceiver;
+
+                cmd.EnableRaisingEvents = true;
+                cmd.Exited += Cmd_ProcessExited; // Handle process completion
+
+                cmd.Start();
+                cmd.BeginOutputReadLine();
+                cmd.BeginErrorReadLine();
+
+                AppendOutput($"Music Download started... Files saved in folder: {myVideosPath}\n");
+
+            }
+
+            catch (Exception ex) 
+            {
+                MessageBox.Show($"error during process...: {ex.Message}");        
+            }
+
+        }
+
+#endregion
+
+
+        #region Output and Completion Handling
+        private void Cmd_OutputDataReceiver(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    AppendOutput(e.Data + Environment.NewLine);
+                });
+            }
+        }
+
+        private void Cmd_ProcessExited(object sender, EventArgs e)
+        {
+           
+            {
+                AppendOutput("=====> Download Complete <=====\n");
+
+
+            };
+        }
+
+        private void AppendOutput(string text)
+        {
+            rtbOUTPUT.AppendText(text);
+            rtbOUTPUT.SelectionStart = rtbOUTPUT.Text.Length;
+            rtbOUTPUT.ScrollToCaret();
+        }
+        #endregion
+
+        #region Button Events Terminating
+        private void btnStartDownload_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txbURL.Text))
+            {
+                MessageBox.Show("Please insert a valid YouTube URL...");
+                return;
+            }
+
+            else if (checkMusicOrVideo.Checked == true)
+            {
+                // Video mode
+                StartDownloadVideo();
+            }
+            else if (checkMusicOrVideo.Checked == false) 
+            {
+                // Music mode
+                StartDownloadMusic();
+            }
+        }
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            rtbOUTPUT.Clear();
+            txbURL.Clear();
+        }
+
+        private void btnKillProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmd != null && !cmd.HasExited)
+                {
+                    cmd.Kill();
+                    cmd.Dispose();
+                    MessageBox.Show("Download process terminated.");
+                }
+                else
+                {
+                    MessageBox.Show("No active process to terminate...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while terminating the process: {ex.Message}");
+            }
+        }
+        #endregion
+
+        #region OpenDownloadFolder
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            string myVideosPath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+            string outputFolder = Path.Combine(myVideosPath, "ytdlp_videos");
+
+            if (!Directory.Exists(outputFolder))
+            {
+                MessageBox.Show("Download folder doesn't exist!");
+            }
+            else
+            {
+                Process.Start("explorer.exe", outputFolder);
+            }
+        }
+        #endregion
+        #region UpdateKernel
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            try {
+            //string commandosUpdatos = "yt-dlp --update-to master";
+            string commandosUpdatos = " --update-to master";
+
+            cmd = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "yt-dlp.exe",
+                    Arguments = commandosUpdatos,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                }
+            };
+
+            cmd.OutputDataReceived += Cmd_OutputDataReceiver;
+            cmd.ErrorDataReceived += Cmd_OutputDataReceiver;
+
+            cmd.EnableRaisingEvents = true;
+            cmd.Exited += Cmd_ProcessExited; // Handle process completion
+
+            cmd.Start();
+            cmd.BeginOutputReadLine();
+            cmd.BeginErrorReadLine();
+
+                //
+
+                AppendOutput($"... UPDATE KERNEL IN PROGRESS ...\n");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+
+        //
+
+}
+
+
+
+        #endregion
+
+   
+    }
+}
